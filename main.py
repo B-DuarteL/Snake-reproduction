afafe96304a00152b868171e9f72eab023f3ab7c
abarthelem <andre.barthelemy.halle@gmail.com>
2025-12-05 13:29:36 +0000

    rajout de bases tkinter (fenêtre)

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import random
+import tkinter as tk
 # Di re ct io ns : ( dx , dy )
 NORD = (0 , -1)
 SUD = (0 , 1)
 EST = (1 , 0)
 OUEST = ( -1 , 0)
-def i n i t _ s e r p e n t () :
-" " " In it ia li se le serpent au centre ( longueur 3) .
-" " "
-centre = 10 # Pour une grille 20 x20
-return [( centre , centre ) , ( centre -1 , centre ) , ( centre -2 , centre ) ]
+def init_serpent() :
+    """ Initialise le serpent au centre ( longueur 3) ."""
+    centre = 10 # Pour une grille 20 x20
+    return [( centre , centre ) , ( centre -1 , centre ) , ( centre -2 , centre ) ]
+# 1. Creation de la fenetre principale (Root)
+fenetre = tk.Tk()
+fenetre.title("SNAKE")
+fenetre.geometry("800x600")
